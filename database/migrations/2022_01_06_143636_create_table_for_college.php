6547f40d9735379ce31fb52e8c4b969ef4e22e35
Yogesh <yogacharya7@gmail.com>
2022-01-06 22:15:59 +0545

    migration added

--- database/migrations/2022_01_06_143636_create_table_for_college.php
+++ database/migrations/2022_01_06_143636_create_table_for_college.php
@@ -132,6 +132,162 @@ class CreateTableForCollege extends Migration
                     
         });
 
+        Schema::create('news_notices', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title',200);
+            $table->string('file_upload',500)->nullable();
+            $table->string('description',500)->nullable();
+            $table->unsignedSmallInteger('display_order')->nullable();
+                    
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('fee_structure', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('course_name',200);
+            $table->string('duration',100);
+            $table->string('fees',100);
+            $table->unsignedSmallInteger('display_order')->nullable();
+                    
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('scholarships', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title',200);
+            $table->string('description',500)->nullable();
+            $table->string('file_upload',500)->nullable();
+            $table->unsignedSmallInteger('display_order')->nullable();
+                    
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('agent_details', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('agent_name',200);
+            $table->string('email',50)->nullable();
+            $table->string('phone',50)->nullable();
+            $table->string('website',50)->nullable();
+            $table->string('address',50)->nullable();
+            $table->string('city',50)->nullable();
+            $table->string('country',50)->nullable();
+            $table->string('description',500)->nullable();
+            $table->string('image',500)->nullable();
+            $table->unsignedSmallInteger('display_order')->nullable();
+                    
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('faq', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title',100);
+            $table->string('description',500)->nullable();
+            $table->unsignedSmallInteger('display_order')->nullable();
+                    
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('slider', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title',200);
+            $table->string('image',500)->nullable();
+            $table->string('description',500)->nullable();
+            $table->unsignedSmallInteger('display_order')->nullable();
+
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('human_resource', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('code',100)->nullable();
+            $table->unsignedSmallInteger('type')->nullable();
+            $table->string('name',100);
+            $table->string('description',500)->nullable();
+            $table->string('email',50)->nullable();
+            $table->string('phone',50)->nullable();
+            $table->string('address',50)->nullable();
+            $table->string('degree',50)->nullable();
+            $table->string('institute',50)->nullable();
+            $table->string('image',500)->nullable();
+
+            $table->unsignedSmallInteger('display_order')->nullable();
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('events', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name',100);
+            $table->string('date',10);
+            $table->string('description',500)->nullable();
+
+            $table->unsignedSmallInteger('display_order')->nullable();
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
+
+        Schema::create('class', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name',100);
+            $table->string('capacity',50)->nullable();
+            $table->string('section',50)->nullable();
+            $table->string('description',500)->nullable();
+
+            $table->unsignedSmallInteger('display_order')->nullable();
+            $table->timestamps();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_uq_code')->nullable()->default(1);
+            $table->unsignedInteger('deleted_by')->nullable();
+                    
+        });
 
 
 
