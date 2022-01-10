64ca73ea9b6791c5c2820dfdf3d99d86036c3c43
sandipsilwal <sandip.silwal.ss@gmail.com>
2022-01-10 17:03:34 +0545

    temp

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -38,23 +38,24 @@
                 <!--About Area Start--> 
                 <div class="about-area mt-95">
                     <div class="container">
-                        <div class="row">
-                            <div class="col-lg-7">
-                                <div class="about-container">
-                                    <h3>Provide best <span class="orange-color">education</span> <span class="orange-color">services</span> for you</h3>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam eligendi expedita, provident cupiditate in excepturi.</p>
-                                    <a class="button-default" href="#">Learn Now</a>	      
+                        @foreach ($about_us as $about)
+                            <div class="row">
+                                <div class="col-lg-7">
+                                    <div class="about-container">
+                                        <h3><span class="orange-color">{{$about->title}}</span></h3>
+                                        <p>{{$about->details}}</p>
+                                        {{-- <a class="button-default" href="#">Learn Now</a>	       --}}
+                                    </div>
+                                </div>
+                                <div class="col-lg-5">
+                                    <!--About Image Area Start-->
+                                    <div class="about-image-area img-full">
+                                        <img src="{{url('/') . '/storage/uploads/' . $about->file_upload}}" alt="">
+                                    </div>
+                                    <!--About Image Area End-->
                                 </div>
                             </div>
-                            <div class="col-lg-5">
-                                <!--About Image Area Start-->
-                                <div class="about-image-area img-full">
-                                    <img src="img/about/about1.jpg" alt="">
-                                </div>
-                                <!--About Image Area End-->
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
                 <!--End of About Area-->
