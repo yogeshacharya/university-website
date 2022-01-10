73fbb3df6c51271d8d99c8f72fb33b616da91b95
Yogesh <yogacharya7@gmail.com>
2022-01-10 22:56:48 +0545

    push

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -86,7 +86,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-md-7 col-12">
-                    <span>Copyright &copy; Educat 2017.All right reserved.Created by <a href="#">Edubuzz</a></span>
+                    <span>Copyright &copy; All right reserved.Created by <a href="#">Lincon</a></span>
                 </div>
                 <div class="col-lg-6 col-md-5 col-12">
                     <div class="column-right">
